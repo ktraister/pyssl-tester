285bbb9657c491841be6d4928c83abfde37e3333
Keith Traister <keith.traister@gmail.com>
2018-01-04 17:45:44 -0500

    certgrab will grab a cert from a server and print it!

--- certgrab.py
+++ certgrab.py
@@ -1,1 +1,9 @@
-import
+import ssl
+
+server = input("Which server should I grab a cert from? ")
+print()
+port = input("How about a port? ")
+print()
+
+print(ssl.get_server_certificate((server, port)))
+print()
